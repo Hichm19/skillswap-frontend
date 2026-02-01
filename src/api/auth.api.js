c9ee16cdfb9a62b9ae5cbec351bee5a4f2e60111
hicham <chamackgaba@gmail.com>
2026-02-01 22:35:44 +0100

    intération d'api

---
+++ src/api/auth.api.js
@@ -0,0 +1,13 @@
+import api from "./axios";
+
+export const login = (data)=> {
+    return api.post('/login', data);
+};
+
+export const register =(data) => {
+    return api.post('/register', data);
+};
+
+export const me = () => {
+  return api.get("/me");
+};
